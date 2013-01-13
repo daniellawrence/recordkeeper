ffe34605edbefabe0e8c6370af663778d1aef9e8
Daniel Lawrence <daniel@danielscottlawrence.com>
2013-01-13 16:40:20 +1100

    added placeholder tests. To be replaced with unit testse

---
+++ tests/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import cli
+import time
+#
+
+start = time.time()
+cli.cli_delete_record("name.defined")
+cli.cli_add_record("name=john age=31 sex=male")
+cli.cli_add_record("name=jeff age=52 sex=male")
+cli.cli_add_record("name=fred age=29 sex=male")
+cli.cli_add_record("name=sandy age=22 sex=female")
+cli.cli_add_record("name=bob age=31 sex=male")
+cli.cli_add_record("name=greg age=52 sex=male")
+cli.cli_add_record("name=dan age=29 sex=male")
+cli.cli_add_record("name=sam age=22 sex=female")
+cli.cli_print_record("name.defined name age sex")
+
+#cli.cli_delete_record("age>30")
+cli.cli_update_record("name=fred","age=0")
+cli.cli_print_record("name.defined name age sex")
+#cli.cli_delete_record("age<1")
+cli.cli_print_record("name.defined name age sex")
+
+finish = time.time()
+
+print "took %.5f seconds" % ( finish - start )
+
