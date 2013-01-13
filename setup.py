dcf02b00063d6ccab56872019c40c057ee363c81
Daniel Lawrence <daniel@danielscottlawrence.com>
2013-01-13 16:40:00 +1100

    added basic setup.py to be used with requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RecordKeeper',
+      version='0.0',
+      description='RecordKeeper',
+      author='Danny Lawrence',
+      author_email='dannyla@linux.com',
+      url='http://www.github.com/daniellawrence/recordkeeper',
+      packages=['recordkeeper'],
+      scripts=['bin/rk_delete.py','bin/rk_new.py','bin/rk_print.py','bin/rk_update.py','bin/start_webserver.py'],
+)
