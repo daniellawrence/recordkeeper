728fa32bbbbf0dcf98a1d1d158f01caeace67f49
Daniel Lawrence <dannyla@linux.com>
2013-02-20 23:20:10 +1100

    PEP8

--- recordkeeper/web.py
+++ recordkeeper/web.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
+"""
+This is a horrible, horrible way of generating a web gui for this.
+It needs some love.
+Would be really good to move this into a more MVC style with jinja2 templates.
+"""
+
 from flask import Flask, jsonify
 app = Flask(__name__)
 
 import api
 import rc_exceptions
 import settings
+import re
+import datetime
+from bson.objectid import ObjectId
+
 
 def debug(msg):
+    """ If debug is enabled in the settings or local_settings.py then print a
+    debug message to the console.
+    """
     if settings.DEBUG:
         print("DEBUG: web.%(msg)s" % locals())
 
 
 def encode_model(obj, recursive=False):
-    import datetime
-    from bson.objectid import ObjectId 
+    """ Take a response from mongodb and turn it in to a json compilable object
+    """
 
     if obj is None:
         #return obj
@@ -23,7 +36,7 @@ def encode_model(obj, recursive=False):
     elif isinstance(obj, (list)):
         out = [encode_model(item) for item in obj]
     elif isinstance(obj, (dict)):
-        out = dict([(k,encode_model(v)) for (k,v) in obj.items()])
+        out = dict([(k, encode_model(v)) for (k, v) in obj.items()])
     elif isinstance(obj, datetime.datetime):
         out = str(obj)
     elif isinstance(obj, ObjectId):
@@ -34,36 +47,37 @@ def encode_model(obj, recursive=False):
     elif isinstance(obj, float):
         out = str(obj)
     else:
-        raise TypeError, "Could not JSON-encode type '%s': %s" % (type(obj), str(obj))
+        raise TypeError(
+           "Could not JSON-encode type '%s': %s" % (type(obj), str(obj))
+        )
     return out
 
 
 @app.route("/")
 def index():
-    format_list = ['html','json']
-    response =[]
+    format_list = ['html', 'json']
+    response = []
     for format in format_list:
         response.append("<a href='/%(format)s'>%(format)s</a>" % locals())
-    return "<Br />".join ( response)
+    return "<Br />".join(response)
 
 
 @app.route("/<format>/")
 def format_index(format):
     record_list = api.find_records({})
-    if format in ['python','json']:
+    if format in ['python', 'json']:
         json_data = {'record_list': encode_model(record_list), 'query': {}}
         print json_data
-        return jsonify( json_data )
+        return jsonify(json_data)
 
     response = []
     for record in record_list:
         response.append("<a href='/html/record/%(name)s'>%(name)s</a> - <a href='/html/delete_record/%(name)s'>delete %(name)s</a>" % record)
-    return "<Br />".join ( response)
-
+    return "<Br />".join(response)
 
 
 @app.route("/<format>/search/<query>")
-def query_records(format,query,display_fields=None):
+def query_records(format, query, display_fields = None):
     debug("query_records(format='%(format)s' query='%(query)s' display_fields=%(display_fields)s)" % locals())
     if '&&' in query:
         query = " ".join
@@ -71,19 +85,20 @@ def query_records(format,query,display_fields=None):
         record_list = api.find_records(query)
         record_list_count = len(record_list)
     except rc_exceptions.NoRecordsFound as error:
-        return "%(error)s" % locals()
+        return "%s" % error
 
-
-
-    if format in ['python','json']:
-        json_data = {'record_list': encode_model(record_list), 'query': query, 'display_fields': display_fields, 'count': record_list_count}
-        print json_data
-        return jsonify( json_data )
+    if format in ['python', 'json']:
+        json_data = {
+            'record_list': encode_model(record_list),
+            'query': query,
+            'display_fields': display_fields,
+            'count': record_list_count
+        }
+        return jsonify(json_data)
 
     simple_format = None
     if display_fields:
-        import re
-        simple_format = re.sub('(?P<m>\w+)',"%(\g<m>)s", " ".join(display_fields) )
+        simple_format = re.sub('(?P<m>\w+)',"%(\g<m>)s", " ".join(display_fields))
 
     response = []
     for record in record_list:
@@ -92,32 +107,33 @@ def query_records(format,query,display_fields=None):
             response.append("<a href='/html/record/%(name)s'>show %(name)s</a> - <a href='/html/delete_record/%(name)s'>delete %(name)s</a>" % record + key_data)
             continue
         response.append("<a href='/html/record/%(name)s'>show %(name)s</a> - <a href='/html/delete_record/%(name)s'>delete %(name)s</a>" % record)
-    return "<Br />".join ( response)
+    return "<Br />".join(response)
+
 
 @app.route("/<format>/search/<query>/<display_fields>")
 def query_records_with_display_fields(format,query, display_fields):
-    debug("qrwdf(format=%(format)s query=%(query)s display_fields=%(display_fields)s" % locals())
+    debug("format=%(format)s query=%(query)s display_fields=%(display_fields)s" % locals())
     display_fields = display_fields.split(',')
-    query += " name " + " ".join( display_fields )
-    return query_records(format=format, query=query, display_fields=display_fields)    
-
+    query += " name " + " ".join(display_fields)
+    return query_records(format = format, query = query, display_fields = display_fields)    
 
 
 @app.route("/<format>/record/<record_name>")
-def find_record(format,record_name):
+def find_record(format, record_name):
     try:
         record_list = api.find_records({'name': record_name})
     except rc_exceptions.NoRecordsFound as error:
-        return "%(error)s" % locals()
+        return str(error)
 
-    if format in ['python','json']:
-        json_data =  encode_model(record_list[0])
-        return jsonify( json_data )
+    if format in ['python', 'json']:
+        json_data = encode_model(record_list[0])
+        return jsonify(json_data)
     response = []
     for key, value in record_list[0].items():
         if value:
             response.append("<strong>%(key)s:</strong> %(value)s" % locals())
-    return "<Br />".join ( response)
+    return "<Br />".join(response)
+
 
 @app.route("/<format>/add_record/<record_data>")
 def insert_record(format, record_data):
@@ -125,8 +141,9 @@ def insert_record(format, record_data):
     try:
         api.insert_record(record_data)
     except rc_exceptions.DuplicateRecord as error:
-        return "%(error)s" % locals()
+        return str(error)
     return query_records(format=format, query=record_data)
+
 
 @app.route("/<format>/delete_record/<query>")
 def delete_record(format, query):
@@ -134,8 +151,9 @@ def delete_record(format, query):
     try:
         api.delete_record({'name': query})
     except rc_exceptions.DuplicateRecord as error:
-        return "%(error)s" % locals()
+        return str(error)
     return "deleted: %(query)s" % locals()
+
 
 @app.route("/<format>/update_record/<record_data>/where/<database_query>")
 def update_record(format, record_data, database_query):
@@ -143,8 +161,6 @@ def update_record(format, record_data, database_query):
     api.update_record(database_query, record_data)
     return "updated record"
     #return query_records(format=format, query=database_query)
-
-
 
 if __name__ == "__main__":
     app.debug = True
