2398fb74af024ba93b48e55e7684cd003b7fd8a3
Daniel Lawrence <dannyla@linux.com>
2013-02-20 23:54:50 +1100

    Updated for PEP

--- recordkeeper/api.py
+++ recordkeeper/api.py
@@ -3,13 +3,16 @@ import db
 import settings
 import rc_exceptions
 
-OPERATOR_LIST = ['=','!','<','>','~','.defined','.undefined','.in.','.nin.', \
-                 '.not.', '.gt.','.lt.', '.ss.']
+# List of all the 'operators' that a user is allow to do.
+OPERATOR_LIST = ['=', '!', '<', '>', '~', '.defined', '.undefined', '.in.',
+                 '.nin.', '.not.', '.gt.', '.lt.', '.ss.']
+                 
 
 def debug(msg):
     """ print a debug message. """
     if settings.DEBUG:
         print "DEBUG: api.%(msg)s" % locals()
+
 
 def field_divider(field_list):
     """ This will take a list of fields and split them into database_query or
@@ -24,7 +27,7 @@ def field_divider(field_list):
 
     debug( "field_divider(field_list=%(field_list)s<%(field_list_type)s>)" % locals())
 
-    if type(field_list).__name__ in ['str','unicode']:
+    if type(field_list).__name__ in ['str', 'unicode']:
         if ' ' in field_list:
             debug( "field_divider: cutting field_list='%(field_list)s'" % locals())
             import shlex
@@ -57,17 +60,16 @@ def field_divider(field_list):
         else:
             display_field_list.append(field)
 
-    #for display_field in display_field_list:
-        #cli_query_list.append("%(display_field)s.defined" % locals())
-
-    debug( "field_divider: cli_query_list=%(cli_query_list)s" % locals())
-    debug( "field_divider: display_field_list=%(display_field_list)s" % locals())
+    debug("field_divider:cli_query_list=%(cli_query_list)s" % locals())
+    debug("field_divider:display_field_list=%(display_field_list)s" % locals())
     return cli_query_list, display_field_list
+
 
 def get_display_fields(field_list):
     """ Given a field_list, only return the display fields in a list. """
     cli_query_list, display_field_list = field_divider(field_list)
     return display_field_list
+
 
 def get_query_fields(field_list):
     """ Given a field_list, only return the display fields in a list. """
@@ -75,42 +77,39 @@ def get_query_fields(field_list):
     return cli_query_list
 
 
-
-def generate_database_multi_update( cli_query_list ):
+def generate_database_multi_update(cli_query_list):
+    """ This allows the CLI to be able to send more than a single key, value for 
+    updates in a single statement.
+    """
 
     if type(cli_query_list).__name__ == 'str':
         cli_query_list = cli_query_list.split(' ')
 
     update_dict = {'$set': {}}
     for cli_query in cli_query_list:
-        print "generate_database_multi_update: %(cli_query)s" % locals()
+        debug("generate_database_multi_update: %(cli_query)s" % locals())
         if '=' in cli_query:
             (key, value) = cli_query.split('=')
-            update_dict['$set'].update( {'%(key)s' % locals(): value } )
-
-        # pushAll and popAll have been removed because the logic to convert a single
-        # item into a list ( aka, string to list, then update ), has not been completed
-        # once this is completed we should be able to do more list logic.
-        #if '+' in cli_query:
-        #    (key, value) = cli_query.split('+')
-        #    update_dict['$pushAll'].update( {"%(key)s" % locals(): value})
-        #if '-' in cli_query:
-        #    (key, value) = cli_query.split('+')
-        #    update_dict['$popAll'].update( {"%(key)s" % locals(): value})
+            update_dict['$set'].update({'%(key)s' % locals(): value})
 
     if not update_dict['$set']:
         del update_dict['$set']
 
-    #if not update_dict['$pushAll']:
-    #    del update_dict['$pushAll']
-
     return update_dict
 
-def process_subquery( cli_query ):
+
+def process_subquery(cli_query):
     """ If the cli_query had a subquery then this is the place that will sort
     out that mess for you.
-    TODO: HORRBILE NEEDS A REFACTOR!
-    TODO: NOT TESTED
+    This allows the user to do crazy queries that have nested quires inside of
+    them.
+    eg.
+    Find all the records where the 'age' of the record is the same as the record
+    with the name of 'john'.
+
+        age=(name=john)
+
+    TODO: HORRBILE RE NEEDS A REFACTOR!
     """
     import re
     querydict = re.search('(?P<key>\w+)(?P<op>.*)\((?P<subkey>\w+)(?P<subop>\W+)(?P<subval>\w+)\)',cli_query).groupdict()
@@ -119,70 +118,80 @@ def process_subquery( cli_query ):
     operator = querydict['op']
     query = None
 
+    #  split up the subquery in the key, operator and value.
     sq_key, sq_op, sq_value = get_key_operator_value_from_cli_query(subquery)
-    sq_database_query = generate_database_query( sq_key, sq_op, sq_value )
+    # Generate the mongodb query that will be used to gather all the subquery
+    # results.
+    sq_database_query = generate_database_query(sq_key, sq_op, sq_value)
 
-
+    # Grab all the subquery results from the database.
     rc = db.RecordKeeper()
     sq_record_list = rc.find(sq_database_query, [key])
 
     sq_values_list = []
 
+    # For each result in the subquery, loop over the record and only grab out 
+    # The value of the key that the query was based on.
+    # In the example of:
+    #     age=(name=john)
+    # This will be grabbing the value of 'age' for all records that match:
+    #     name=john
     for sq_value in sq_record_list:
         try:
-            sq_values_list.append( sq_value[key] )
+            sq_values_list.append(sq_value[key])
         except KeyError as error:
             print "%(error)s - %(sq_value)s %(key)s" % locals()
 
+    # The number of results is important to work out how to generate the query.
+    # If there is only 1 result, then we can do an equals comparison.
+    # If there is more then 1 result, then we need to do an "in" comparison.
     number_of_sq_values = len(sq_values_list)
-
 
     if number_of_sq_values == 1:
         sq_value = sq_values_list[0]
-        query="%(key)s%(operator)s%(sq_value)s" % locals()
+        query = "%(key)s%(operator)s%(sq_value)s" % locals()
 
-    if number_of_sq_values > 1 and operator in [ "=", '.in.' ]:
+    if number_of_sq_values > 1 and operator in ["=", '.in.']:
         filtered_sub_result = []
         for v in sq_values_list:
-            filtered_sub_result.append( str(v) )
-        query="%s.in.%s" % ( key, ",".join(filtered_sub_result) )
+            filtered_sub_result.append(str(v))
+        query = "%s.in.%s" % (key, ",".join(filtered_sub_result))
 
-    if number_of_sq_values > 1 and operator in [ "!", '.nin.' ]:
+    if number_of_sq_values > 1 and operator in ["!", '.nin.']:
         filtered_sub_result = []
         for v in sq_values_list:
-            filtered_sub_result.append( str(v) )
-        query="%s.nin.%s" % ( key, ",".join(filtered_sub_result) )
+            filtered_sub_result.append(str(v))
+        query = "%s.nin.%s" % (key, ",".join(filtered_sub_result))
 
+    # If the query was a greater then operation, then we just need to grab
+    # the highest value that was generated by the sub-query.
+    if number_of_sq_values > 1 and operator in ['.gt.', '>']:
+        filtered_sub_result = []
+        for v in sq_values_list:
+            try:
+                filtered_sub_result.append(float(v))
+            except ValueError:
+                continue
+        max_value = max(filtered_sub_result)
+        query = "%s>%s" % (key, max_value)
 
-    if number_of_sq_values > 1 and operator in ['.gt.','>']:
-       filtered_sub_result = []
-       for v in sq_values_list:
-           try:
-              filtered_sub_result.append( float(v) )
-           except ValueError:
-              continue
-       max_value = max(filtered_sub_result)
-       query="%s>%s" % ( key, max_value )
+    # If the query was a lower then operation, then we just need to grab
+    # the lowest value that was generated by the sub-query.
+    if number_of_sq_values > 1 and operator in ['.lt.', '<']:
+        filtered_sub_result = []
+        for v in sq_values_list:
+            try:
+                filtered_sub_result.append(float(v))
+            except ValueError:
+                continue
+        min_value = min(filtered_sub_result)
+        query = "%s<%s" % (key, min_value)
 
-    if number_of_sq_values > 1 and operator in ['.lt.','<']:
-       filtered_sub_result = []
-       for v in sq_values_list:
-           try:
-              filtered_sub_result.append( float(v) )
-           except ValueError:
-              continue
-       min_value = min(filtered_sub_result)
-       query="%s<%s" % ( key, min_value )
-
-
-    print "Turned cli_query='%(cli_query)s' into '%(query)s'" % locals()
-
+    debug("Turned cli_query='%(cli_query)s' into '%(query)s'" % locals())
     return query
 
 
-
-
-def get_key_operator_value_from_cli_query( cli_query ):
+def get_key_operator_value_from_cli_query(cli_query):
     """ Work out the key, operator and the value from a cli_query. """
 
     debug("get_kov: %(cli_query)s" % locals())
@@ -202,50 +211,54 @@ def get_key_operator_value_from_cli_query( cli_query ):
         raise NotImplementedError(
             "Was not able to find a query_operator: %(cli_query)s" % locals())
 
-
-    if query_operator in ['.defined','.undefined']:
+    if query_operator in ['.defined', '.undefined']:
         key = cli_query.split('.')[0]
         value = None
     else:
-        (key, value) = cli_query.split( query_operator )
+        (key, value) = cli_query.split(query_operator)
 
-    # if the type of value is a string, and it 
-    if type(value).__name__ in [ "str", 'unicode' ] and value.lower() == "true":
+    value_type = type(value).__name__
+
+    # If the value is true or True, then convert it to a boolean
+    if value_type in ["str", 'unicode'] and value.lower() == "true":
         value = True
 
-    # if the type of value is a string, and it 
-    if type(value).__name__  in [ "str", 'unicode' ]  and value.lower() == "false":
+    # If the value is false or False, then convert it to a boolean
+    if value_type in ["str", 'unicode'] and value.lower() == "false":
         value = False
 
-    if type(value).__name__  in [ "str", 'unicode' ]  and  value.isdigit():
+    # If the value is a digit, then convert it to an int.
+    if value_type in ["str", 'unicode'] and value.isdigit():
         value = int(value)
 
-    debug("get_kov: %s<%s>" % ( value, type(value).__name__ ))
+    debug("get_kov: %s<%s>" % (value, value_type))
 
     return key, query_operator, value
 
 
-def generate_database_query( key, query_operator, value ):
+def generate_database_query(key, query_operator, value):
     """ Take a single query (eg. os=solaris) and generate a json/dict style
     filter ready for mongodb.
-    The filter that is returned is a python dict.
+    The filter that is returned is a python dict...
+        os=solaris  becomes {'os': 'solaris'}
+        os!=solaris becomes {'os': {'$ne': 'solaris'}}
     """
 
     if query_operator == '.defined':
-        return {'%s' % key: {'$exists': True }}
+        return {'%s' % key: {'$exists': True}}
 
     # If the query_operator is 'undefined', create the $exists query
     if query_operator == '.undefined':
-        return {'%s' % key: {'$exists': False }}
+        return {'%s' % key: {'$exists': False}}
 
     # If query is an = then simple operator
     if query_operator == "=":
         return {"%s" % key: value}
 
     # db.customers.find( { name : { $not : /acme.*corp/i } } );
-    # { key: { $not: value } } 
+    # { key: { $not: value }} 
     if query_operator == "!" or query_operator == ".not.":
-        return {"%s" % key: {'$ne': value }}
+        return {"%s" % key: {'$ne': value}}
 
     # db.myCollection.find( { a : { $gt: 3 } } );
     # { key: { $gt: value } } 
@@ -315,6 +328,9 @@ def generate_database_multi_query( cli_query_list ):
 
 
 def find_records(field_list):
+    """ Given a list of fields, find all the records that match queries in the
+    field_list.
+    """
     debug("find_records(%s)" % field_list)
 
     database_query = None
@@ -350,6 +366,7 @@ def find_records(field_list):
     record_list = rc.find(database_query, display_field_list)
     return record_list
 
+
 def insert_record(record_data):
     """ accept a string of key=value pairs or a dict() of {key: value} pairs and
     add them to the records database.
@@ -376,6 +393,7 @@ def insert_record(record_data):
     new_record = rc.insert( database_query )
     return new_record
 
+
 def delete_record(field_list, force=False):
     #cli_query_list, display_field_list = field_divider(field_list)
     #database_query = generate_database_multi_query( cli_query_list )
@@ -390,18 +408,15 @@ def delete_record(field_list, force=False):
     if type(field_list).__name__ in ['dict']:
         database_query = field_list
 
-
-
     if not database_query:
         raise rc_exceptions.InvaildQuery("Didn't build a query from '%(field_list)s'" % locals())
 
-
     rc = db.RecordKeeper()
-    rc.remove( database_query, force=force )
+    rc.remove(database_query, force = force)
 
 
 def update_record(field_list, record_data):
-    """ accept a field_list and record_data. 
+    """ accept a field_list and record_data.
     All records that match the queries in field_list will be updated with
     record_data.
     """
@@ -416,16 +431,19 @@ def update_record(field_list, record_data):
     rc = db.RecordKeeper()
     rc.update( database_query, record_data_dict )
 
+
 def saved_queries_list():
     """ Return a list of saved queries. """
     rc = db.RecordKeeper()
     query_list = rc.saved_queries_list()
     return query_list
 
+
 def saved_queries_add(query_name, query_data=None):
     """ Add a new saved query. """
     rc = db.RecordKeeper()
     rc.saved_queries_add(query_name=query_name, query_data=query_data)
+
 
 def saved_queries_get(query_name):
     """ get a saved query. """
